e3cb078ca7ac5ca982cd33c86e104a48977c4495
Matthew Walker <matthew.r.walker4@gmail.com>
2021-09-20 17:11:05 -0400

    added first and last names to users

--- src/main/java/com/codeup/springblog/models/User.java
+++ src/main/java/com/codeup/springblog/models/User.java
@@ -26,8 +26,18 @@ public class User {
 
     @Column(nullable = false)
     @NotBlank
-    @Size(min = 8, max = 50, message = "Password must be at least 8 chars in length")
+    @Size(min = 8, max = 100, message = "Password must be at least 8 chars in length")
     private String password;
+
+    @Column(nullable = false, length = 20)
+    @NotBlank
+    @Size(max = 20, message = "must not be longer than 20 characters")
+    private String firstName;
+
+    @Column(nullable = false, length = 30)
+    @NotBlank
+    @Size(max = 30, message = "must not be longer than 30 characters")
+    private String lastName;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<Ad> ads;
@@ -35,11 +45,13 @@ public class User {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<Post> posts;
 
-    public User(long id, String username, String email, String password, List<Ad> ads, List<Post> posts) {
+    public User(long id, String username, String email, String password, String firstName, String lastName, List<Ad> ads, List<Post> posts) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.ads = ads;
         this.posts = posts;
     }
@@ -52,6 +64,8 @@ public class User {
         email = copy.email;
         username = copy.username;
         password = copy.password;
+        firstName = copy.firstName;
+        lastName = copy.lastName;
         ads = copy.ads;
         posts = copy.posts;
     }
@@ -93,6 +107,22 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public List<Ad> getAds() {
